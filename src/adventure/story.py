9e13f94a15a7d6c384edfed6c208f2cd1c2784cd
Saad Ahmad <150076462+swahmad2005@users.noreply.github.com>
2025-10-28 17:28:12 +0000

    Stylized story

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -1,5 +1,7 @@
 from adventure.utils import read_events_from_file
 import random
+from rich.prompt import Prompt
+from rich import print
 
 def step(choice: str, events):
     random_event = random.choice(events)
@@ -9,20 +11,20 @@ def step(choice: str, events):
     elif choice == "right":
         return right_path(random_event)
     else:
-        return "You stand still, unsure what to do. The forest swallows you."
+        return "[bright_red]You stand still, unsure what to do. The forest swallows you.[/bright_red]"
 
 def left_path(event):
-    return "You walk left. " + event
+    return "[bright_cyan][b]You walk left.[/b] " + event + "[/bright_cyan]"
 
 def right_path(event):
-    return "You walk right. " + event
+    return "[green3][b]You walk right.[/b] " + event + "[/green3]"
 
 if __name__ == "__main__":
     events = read_events_from_file('events.txt')
 
-    print("You wake up in a dark forest. You can go left or right.")
+    print("[grey37]You wake up in a dark forest. You can go left or right.[/grey37]")
     while True:
-        choice = input("Which direction do you choose? (left/right/exit): ")
+        choice = Prompt.ask("[b]\nWhich direction do you choose? ([i]left[/i]/[i]right[/i]/[i]exit[/i])[/b]")
         choice = choice.strip().lower()
         if choice == 'exit':
             break
